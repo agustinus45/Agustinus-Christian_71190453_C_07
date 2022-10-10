2284dc1d65ab488ca2dcc90f81e0947317f622e4
agustinus45 <112394610+agustinus45@users.noreply.github.com>
2022-10-10 20:11:18 +0700

    Add files via upload

--- Unguided07.py
+++ Unguided07.py
@@ -31,44 +31,48 @@ class PrefixConverter:
     def infixToPrefix(self,expression):
         operands = []
 		for i in range(len(expression)):
-        if (expression[i] == '('):
-            self.stackList.append(expression[i])
-        elif (expression[i] == ')'):
-            while (len(self.stackList)!=0 and self.stackList[-1] != '('):
-                op1 = operands[-1]
-                operands.pop()
+            if (expression[i] == '('):
+                self.stackList.append(expression[i])
+            elif (expression[i] == ')'):
+                while (len(self.stackList)!=0 and self.stackList[-1] != '('):
+                    op1 = operands[-1]
+                    operands.pop()
 
-                op2 = operands[-1]
-                operands.pop()
+                    op2 = operands[-1]
+                    operands.pop()
 
-                op = self.stackList[-1]
-                self.stackList.pop()
+                    op = self.stackList[-1]
+                    self.stackList.pop()
 
-                tmp = op + op2 + op1
-                operands.append(tmp)
+                    tmp = op + op2 + op1
+                    operands.append(tmp)
 
-            operators.pop()
-
-        elif (not cekValidExpression(expression[i])):
-            operands.append(expression[i] + "")
-
-        else:
-            while (len(self.stackList)!=0 ):
-                op1 = operands[-1
-                operands.pop()
-
-                op2 = operands[-1]
-                operands.pop()
-
-                op = self.stackList[-1]
                 operators.pop()
 
-                tmp = op + op2 + op1
-                operands.append(tmp)
-            self.stackList.append(expression[i])
+            elif (not cekValidExpression(expression[i])):
+                operands.append(expression[i] + "")
+
+            else:
+                while (len(self.stackList)!=0 ):
+                    op1 = operands[-1]
+                    operands.pop()
+
+                    op2 = operands[-1]
+                    operands.pop()
+
+                    op = self.stackList[-1]
+                    operators.pop()
+
+                    tmp = op + op2 + op1
+                    operands.append(tmp)
+                self.stackList.append(expression[i])
 		
 if __name__ == '__main__':
     expresi1 = PrefixConverter()
-
+    print(expresi1.infixToPrefix('1+2+3*4/2-1'))
+    print(expresi1.infixToPrefix('A * (B + C) / D'))
+    print(expresi1.infixToPrefix(''))
+    print(expresi1.infixToPrefix())
+    print(expresi1.infixToPrefix())
 
     
